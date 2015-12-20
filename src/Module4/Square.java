4f4578f6dcf0dc265611e93e0a1612b8a25437e0
VladimirPenkov <corner85@list.ru>
2015-12-20 15:22:44 +0200

    new file:   Module4/Distance.java 	new file:   Module4/Square.java 	new file:   Module4/TemperatureConversion.java

---
+++ src/Module4/Square.java
@@ -0,0 +1,19 @@
+package Module4;
+
+/**
+ * Created by vladi_000 on 12/20/2015.
+ */
+public class Square {
+
+    public static double squareTriangle(double h, double a){
+        return h*a/2;
+    }
+
+    public static double squareCircle(double r){
+        return Math.pow(r,2)*Math.PI;
+    }
+
+    public static double squareRectangle(double a, double b){
+        return a*b;
+    }
+}
