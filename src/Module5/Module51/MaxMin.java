cd7f93eb5e496f9ce1b00639e16087dadf191012
VladimirPenkov <corner85@list.ru>
2015-12-25 10:30:02 +0200

    Module 5. Home tasks

---
+++ src/Module5/Module51/MaxMin.java
@@ -0,0 +1,44 @@
+package Module5.Module51;
+
+/**
+ * Created by corner on 23.12.2015.
+ */
+public class MaxMin {
+
+    public static int max(int... arr) {
+        try {
+            int max = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < max) {
+                    max = arr[i];
+                }
+            }
+            return max;
+        } catch (NullPointerException e) {
+            System.out.println("Array is null");
+            throw e;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Array is empty");
+            throw e;
+        }
+    }
+
+    public static int min(int... arr) {
+        try {
+            int min = arr[0];
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < min) {
+                    min = arr[i];
+                }
+            }
+            return min;
+        } catch (NullPointerException e) {
+            System.out.println("Array is null");
+            throw e;
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Array is empty");
+            throw e;
+        }
+    }
+
+}
---
+++ src/Module5/Module52/SortType.java
@@ -0,0 +1,17 @@
+package Module5.Module52;
+
+/**
+ * Created by corner on 24.12.2015.
+ */
+public enum SortType {
+    BUBBLE,
+    SELECTION,
+    INSERTION,
+    HEAP,
+    MERGE,
+    QUICKSORT;
+
+    public static SortType getRandom() {
+        return values()[(int) (Math.random() * values().length)];
+    }
+}
