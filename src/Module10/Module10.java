9691c65e76e858417d032b19b4e8d034fc49c562
VladimirPenkov <corner85@list.ru>
2016-01-28 23:11:34 +0200

    Module 10.

--- src/Module10/Module10.java
+++ src/Module10/Module10.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.Scanner;
 
 /**
- * Created by corner on 28.01.2016.
+ * Created by corner  on 28.01.2016.
  */
 public class Module10 {
 
@@ -25,57 +25,42 @@ public class Module10 {
         final Scanner scanner = new Scanner(System.in);
         String enteredRow = null;
         String fileName = null;
-        while (!(enteredRow = enterWithDescription(scanner, "Enter command (READ,WRITE,EXIT): ")).toUpperCase().equals(commandExit)) {
+        try {
+            while (!(enteredRow = enterWithDescription(scanner, "Enter command (READ,WRITE,EXIT): ")).toUpperCase().equals(commandExit)) {
 
-            if (enteredRow.toUpperCase().equals(commandWrite)) {
-                fileName = enterWithDescription(scanner, "Enter name of file : ");
-                CustomOutputStream customOutputStream = null;
-                try {
-                    customOutputStream = new CustomOutputStream(new FileWriter(fileName), shiftingOfAlgorithm, amountOfAlphabet);
-                    while (!(enteredRow = enterWithDescription(scanner, "Enter rows(print \\FINISH when the entering will completed): ")).toUpperCase().equals(commandFinish)) {
-                        customOutputStream.write(enteredRow + "\n");
-                    }
-                } catch (IOException e) {
-                    System.out.println("IO exception. Check file");
-                } finally {
+                if (enteredRow.toUpperCase().equals(commandWrite)) {
+                    fileName = enterWithDescription(scanner, "Enter name of file : ");
+                    CustomOutputStream customOutputStream = null;
                     try {
-                        if (customOutputStream != null)
-                        {
+                        customOutputStream = new CustomOutputStream(new FileWriter(fileName), shiftingOfAlgorithm, amountOfAlphabet);
+                        while (!(enteredRow = enterWithDescription(scanner, "Enter rows(print \\FINISH when the entering will completed): ")).toUpperCase().equals(commandFinish)) {
+                            customOutputStream.write(enteredRow + "\n");
+                        }
+                    } finally {
+                        if (customOutputStream != null) {
                             customOutputStream.close();
                         }
-                    } catch (IOException e) {
-                        System.out.println("IO exception. Check file");
                     }
-                }
-            } else if (enteredRow.toUpperCase().equals(commandRead)) {
-                fileName = enterWithDescription(scanner, "Enter name of file : ");
-                CustomInputStream customInputStream = null;
-                try {
-                    customInputStream = new CustomInputStream(new FileReader(fileName), shiftingOfAlgorithm, amountOfAlphabet);
-                    String readline = null;
+                } else if (enteredRow.toUpperCase().equals(commandRead)) {
+                    fileName = enterWithDescription(scanner, "Enter name of file : ");
+                    CustomInputStream customInputStream = null;
                     try {
+                        customInputStream = new CustomInputStream(new FileReader(fileName), shiftingOfAlgorithm, amountOfAlphabet);
+                        String readline = null;
                         while ((readline = customInputStream.readLine()) != null) {
                             System.out.print(readline);
                         }
-                    } catch (IOException e) {
-                        System.out.println("IO exception. Check file");
-                    }
-                } catch (FileNotFoundException e) {
-                    System.out.println("File not found");
-                } finally {
-                    try {
-                        if (customInputStream != null)
-                        {
+                    } catch (FileNotFoundException e) {
+                        System.out.println("File not found");
+                    } finally {
+                        if (customInputStream != null) {
                             customInputStream.close();
                         }
-                    } catch (IOException e) {
-                        System.out.println("IO exception. Check file");
                     }
-
                 }
-
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
     }
 }
